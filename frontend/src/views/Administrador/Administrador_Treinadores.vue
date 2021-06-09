52add914013bb2a9259315d3bc717f638c34c615
Alexandra2899 <xanareigada@gmail.com>
2021-06-09 13:26:07 +0100

    registar treinador

--- frontend/src/views/Administrador/Administrador_Treinadores.vue
+++ frontend/src/views/Administrador/Administrador_Treinadores.vue
@@ -12,7 +12,6 @@
 
         <v-container>
           <v-row align="center" justify="space-around">
-            
             <v-col cols="12" md="8">
               <v-toolbar dense rounded class="mx-6">
                 <v-text-field
@@ -25,8 +24,6 @@
                 ></v-text-field>
               </v-toolbar>
             </v-col>
-            
-     
           </v-row>
         </v-container>
 
@@ -46,85 +43,147 @@
         </v-container>
       </v-col>
 
-      <v-dialog v-model="dialog" persistent max-width="600px">
-      <template v-slot:activator="{ on, attrs }">
-      <v-btn
-        fab
-        dark
-        large
-        fixed
-        right
-        bottom
-        color="#f95738"
-        v-bind="attrs"
-        v-on="on"
-        style="margin-bottom:100px"
-      >
-        <v-icon>mdi-plus</v-icon>
-      </v-btn>
-    </template>
-    <v-card>
-      <v-card-title>
-        Adicionar treinador
-      </v-card-title>
-      <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="12">
-              <v-text-field
-              v-model="data.nome"
-              label="Nome*" 
+      <v-dialog v-model="dialog1" persistent max-width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            fab
+            dark
+            large
+            fixed
+            right
+            bottom
+            color="#f95738"
+            v-bind="attrs"
+            v-on="on"
+            style="margin-bottom: 100px"
+          >
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title> Adicionar treinador </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" md="12">
+                  <v-form class="px-16  mt-6">
+                    <v-text-field
+                      label="Nome Completo*"
+                      name="Name"
+                      v-model="input_register.nome"
+                      prepend-icon="mdi-account"
+                      type="text"
+                      color="#f95738"
+                      required
+                    />
+                    <v-menu
+                      ref="menu"
+                      v-model="menu"
+                      :close-on-content-click="false"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="auto"
+                    >
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                          v-model="input_register.date"
+                          label="Data de Nascimento*"
+                          prepend-icon="mdi-calendar"
+                          readonly
+                          v-bind="attrs"
+                          v-on="on"
+                          color="#f95738"
+                          required
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker
+                        v-model="input_register.date"
+                        no-title
+                        scrollable
+                        color="#7189ff"
+                      >
+                        <v-spacer></v-spacer>
+                        <v-btn text color="#f95738" @click="menu = false">
+                          Cancelar
+                        </v-btn>
+                        <v-btn
+                          text
+                          color="#f95738"
+                          @click="$refs.menu.save(input_register.date)"
+                        >
+                          OK
+                        </v-btn>
+                      </v-date-picker>
+                    </v-menu>
+                    <v-text-field
+                      label="Email*"
+                      name="Email"
+                      v-model="input_register.email"
+                      prepend-icon="mdi-email"
+                      type="text"
+                      color="#f95738"
+                      required
+                    />
+                    <v-text-field
+                      label="Nome de Utilizador*"
+                      name="Username"
+                      v-model="input_register.username"
+                      prepend-icon="mdi-account-lock"
+                      type="text"
+                      color="#f95738"
+                      required
+                    />
+                    <v-text-field
+                      id="password"
+                      label="Palavra-Passe*"
+                      name="password"
+                      v-model="input_register.password"
+                      prepend-icon="mdi-lock"
+                      type="password"
+                      color="#f95738"
+                      required
+                    /><v-select
+                      :items="['Masculino', 'Feminino']"
+                      v-model="input_register.genero"
+                      label="Género"
+                      color="#7189ff"
+                      required
+                    ></v-select>
+                    <v-text-field
+                      label="Breve descrição"
+                      name="descricao"
+                      v-model="input_register.descricao"
+                      type="text"
+                      color="#f95738"
+                      required
+                    />
+                    <v-file-input
+                      id="imagem"
+                      v-model="input_register.imagem"
+                      label="Imagem de Perfil"
+                      name="imagem_perfil"
+                      prepend-icon="mdi-camera"
+                    />
+                    
+                  </v-form>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>* campos obrigatórios</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="#f95738" text @click="dialog1 = false"> Fechar </v-btn>
+            <v-btn
               color="#f95738"
-              required
-              ></v-text-field>
-            
-              <v-text-field
-                v-model="data.username"
-                label="Nome de utilizador*"
-                color="#f95738"
-              ></v-text-field>
-
-              <v-text-field
-                v-model="data.email"
-                label="Email*"
-                color="#f95738"
-              ></v-text-field>
-            
-              <v-text-field
-                v-model="data.password"
-                label="Password*"
-                type="password"
-                color="#f95738"
-                persistent-hint
-                required
-              ></v-text-field>
-        
-            
-              <v-text-field
-                v-model="data.descricao"
-                label="Descrição"
-                color="#f95738"
-                required
-              ></v-text-field>
-            </v-col>
-            
-      
-           
-          </v-row>
-        </v-container>
-        <small>* campos obrigatórios</small>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="#f95738" text @click="dialog = false">
-          Fechar
-        </v-btn>
-        <v-btn color="#f95738" text @click="dialog = false"> Criar </v-btn>
-      </v-card-actions>
-    </v-card>
-
-
-
+              text
+              @click="dialog1 = false"
+              v-on:click="registar()"
+            >
+              Registar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-dialog>
 
       <v-dialog v-model="dialog" persistent max-width="600px">
@@ -143,7 +202,6 @@
             <v-icon>mdi-delete</v-icon>
           </v-btn>
         </template>
-        
       </v-dialog>
     </v-row>
   </div>
@@ -154,6 +212,7 @@
 import NavBar from "@/components/NavBar_Logged.vue";
 import SideBar from "@/components/SideBar_Administrador.vue";
 import axios from "axios";
+import sjcl from "sjcl";
 
 export default {
   name: "ProcurarTreinador",
@@ -164,21 +223,24 @@ export default {
   created() {
     document.title = "Treinadores";
   },
-   mounted(){
+  mounted() {
     axios
-      .get("http://localhost:4576/api/treinador/listar",{headers: {'token': localStorage.getItem("token")}})      
-      .then(response => {
-        this.rows= response.data;
-        console.log("adasdsadsadhhhhhhhhhhhh"+response.data)
-
+      .get("http://localhost:4576/api/treinador/listar", {
+        headers: { token: localStorage.getItem("token") },
       })
-      .catch(e => console.log("erro" +e))
+      .then((response) => {
+        this.rows = response.data;
+        console.log("adasdsadsadhhhhhhhhhhhh" + response.data);
+      })
+      .catch((e) => console.log("erro" + e));
 
-      console.log("dasjdamsjdsnajdzzzzzzzzzzzz");
-  
+    console.log("dasjdamsjdsnajdzzzzzzzzzzzz");
   },
   data() {
     return {
+      dialog1: false,
+      dialog: false,
+      menu: false,
       singleSelect: false,
       selected: [],
       search: "",
@@ -188,19 +250,59 @@ export default {
         { text: "Email", value: "email" },
         { text: "Descrição", value: "descricao" },
       ],
-      rows: [
-
-      ],
-      data: {
-        nome:"",
-        username:"",
-        email:"",
-        password:"",
-        descricao:""
-      }
+      rows: [],
+      input_register: {
+        username: "",
+        password: "",
+        email: "",
+        nome: "",
+        idade: 0,
+        genero: "",
+        foto_perfil: "",
+        descricao: ""
+      },
     };
   },
   methods: {
+    async registar() {
+      function carrega_foto(x) {
+        return new Promise((resolve) => {
+          let blob = new Blob([x]),
+            fileReader = new FileReader();
+          fileReader.readAsArrayBuffer(blob);
+          fileReader.onload = function () {
+            resolve(Buffer.from(this.result).toString("base64"));
+          };
+        });
+      }
+
+      //FALTA VERIFICAR SE O EMAIL E A PASS SAO CORRETOS OU EXISTEM OU QQ COISA ASSIM
+
+      let genero_registo = true;
+      if (this.input_register.genero != "Masculino") {
+        genero_registo = false;
+      }
+      var registoInfo = {
+        email: this.input_register.email,
+        password: sjcl.codec.hex.fromBits(
+          sjcl.hash.sha256.hash(this.input_register.password)
+        ),
+        username: this.input_register.username,
+        nome: this.input_register.nome,
+        data: this.input_register.date,
+        genero: genero_registo,
+        descricao: this.input_register.descricao
+      };
+      registoInfo.foto_perfil = await carrega_foto(this.input_register.imagem);
+
+      axios
+        .post("http://localhost:4576/api/register/treinador", registoInfo)
+        .then((response) => {
+          //const status = JSON.parse(response.status);
+          console.log(response);
+        });
+    },
+
     //verTreino: function (value) {
     //  console.log("ROW VALUES:", value);
     // this.$router.push("/treinos/" + value.codigo);
